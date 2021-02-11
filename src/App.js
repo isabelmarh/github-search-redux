90ee0b5817a6624fa4b75163204884687f09dd39
Isabel Hong <hong.isabel@gmail.com>
2021-02-11 18:18:26 -0300

    changed to redux from context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Navbar, Alert, About } from "./components/layouts";
 import { Users, Search } from "./components/users";
-// import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import User from "./components/users/User";
@@ -11,83 +10,11 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 const App = () => {
-  // const [users, setUsers] = useState([]);
-  // const [user, setUser] = useState({});
-  // const [loading, setLoading] = useState(false);
-  // const [alert, setAlert] = useState(null);
-  // const [repos, setRepos] = useState([]);
-
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     try {
-  //       setLoading(true);
-  //       const res = await axios.get("https://api.github.com/users");
-  //       setUsers(res.data);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       setLoading(false);
-  //       showAlert("Error", "danger");
-  //     }
-  //   };
-  //   fetch();
-  // }, []);
-
-  // const searchUsers = async (text) => {
-  //   try {
-  //     setLoading(true);
-  //     const res = await axios.get(
-  //       `https://api.github.com/search/users?q=${text}`
-  //     );
-  //     setUsers(res.data.items);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     setLoading(false);
-  //     showAlert("Error", "danger");
-  //   }
-  // };
-
-  // const getUser = async (login) => {
-  //   try {
-  //     setLoading(true);
-  //     const res = await axios.get(`https://api.github.com/users/${login}`);
-  //     setUser(res.data);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     setLoading(false);
-  //     showAlert("Error", "danger");
-  //   }
-  // };
-
-  // const getUserRepos = async (login) => {
-  //   try {
-  //     setLoading(true);
-  //     const res = await axios.get(
-  //       `http://api.github.com/users/${login}/repos?per_page=5&sort=created:asc`
-  //     );
-  //     setRepos(res.data);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     setLoading(false);
-  //     showAlert("Error", "danger");
-  //   }
-  // };
-
-  // const clearUsers = () => {
-  //   setUsers([]);
-  // };
-
-  // const showAlert = (msg, type) => {
-  //   setAlert({ msg, type });
-  //   setTimeout(() => {
-  //     setAlert(null);
-  //   }, 3000);
-  // };
-
   return (
     <Provider store={store}>
       <Router>
         <div className="App">
-          <Navbar title="Mansoor's App" icon="fab fa-twitter" />
+          <Navbar title="Github App" icon="fab fa-twitter" />
           <div className="container">
             <Alert />
             <Switch>
@@ -96,12 +23,7 @@ const App = () => {
                 path="/"
                 render={() => (
                   <>
-                    <Search
-                    // searchUsers={searchUsers}
-                    // clearUsers={clearUsers}
-                    // showClear={users.length > 0}
-                    // setAlert={showAlert}
-                    />
+                    <Search/>
                     <Users />
                   </>
                 )}
@@ -110,14 +32,7 @@ const App = () => {
               <Route
                 path="/user/:login"
                 render={(props) => (
-                  <User
-                    {...props}
-                    // getUser={getUser}
-                    // getUserRepos={getUserRepos}
-                    // loading={loading}
-                    // user={user}
-                    // repos={repos}
-                  />
+                  <User {...props}/>
                 )}
               />
             </Switch>
